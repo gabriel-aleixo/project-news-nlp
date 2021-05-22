d2c44a1d4ae738b36dc604949021c052f28b49f5
gabriel-aleixo <63018864+gabriel-aleixo@users.noreply.github.com>
2021-05-21 17:31:23 -0700

    add sass loader, import styles (BOILERPLATE)

--- webpack.dev.js
+++ webpack.dev.js
@@ -11,6 +11,10 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
                 test: /\.(png|jpe?g|gif)$/i,
                 loader: 'file-loader',
                 options: {
