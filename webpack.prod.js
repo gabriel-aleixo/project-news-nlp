87cdac9b09bf78527ac2a102e174cab0ed379cc1
gabriel-aleixo <63018864+gabriel-aleixo@users.noreply.github.com>
2021-05-21 17:01:03 -0700

    add file loader to webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -8,6 +8,23 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.(png|jpe?g|gif)$/i,
+                loader: 'file-loader',
+                options: {
+                    outputPath: 'images',
+                    name(resourcePath, resourceQuery) {
+                        // `resourcePath` - `/absolute/path/to/file.js`
+                        // `resourceQuery` - `?foo=bar`
+
+                        if (process.env.NODE_ENV === 'development') {
+                            return '[path][name].[ext]';
+                        }
+
+                        return '[contenthash].[ext]';
+                    },
+                },
+            },
+            {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
